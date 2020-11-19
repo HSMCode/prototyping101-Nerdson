aa93cca673a74780917140b82083637340906aac
Toni <black_jack_21@gmx.de>
2020-11-19 20:37:38 +0100

    add turnSpeed

--- prototyping101/Assets/Scripts/PlayerController.cs
+++ prototyping101/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 25; // Speed for our hot red vehicle
-
+    public float turnSpeed = 45; // Turn speed for our red hot vehicle
+    public float verticalInput; // Input of arrow keys up and down
+    public float horizontalInput; // Input of arrow keys left and right
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // move our hot red vehicle
-        transform.Translate( translation: Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
