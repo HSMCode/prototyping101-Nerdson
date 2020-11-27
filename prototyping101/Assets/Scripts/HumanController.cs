80e986362b157bc902bb143d9dd73496d18fe215
Toni <black_jack_21@gmx.de>
2020-11-27 13:14:24 +0100

    add HumanController Script
    let's human jump with RETURN

---
+++ prototyping101/Assets/Scripts/HumanController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    private Rigidbody _humanRb;
+    public float jumpForce = 10f;
+    public float gravityModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _humanRb = GetComponent<Rigidbody>();
+        Physics.gravity = Physics.gravity * gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            _humanRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
